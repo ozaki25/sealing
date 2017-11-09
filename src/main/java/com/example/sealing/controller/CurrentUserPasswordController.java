db362743733d421cbf93dd21869af5ad1ae3221f
ozaki <fs65ugumori@yahoo.co.jp>
2017-11-09 22:46:29 +0900

    HTTPメソッドを正確に

--- src/main/java/com/example/sealing/controller/CurrentUserPasswordController.java
+++ src/main/java/com/example/sealing/controller/CurrentUserPasswordController.java
@@ -5,7 +5,7 @@ import java.security.Principal;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -23,7 +23,7 @@ public class CurrentUserPasswordController {
         return "current_user/password/edit";
     }
 
-    @PatchMapping
+    @PutMapping
     public String update(Principal principal, RedirectAttributes attributes, @RequestParam String password, @RequestParam String passwordConfirm) {
         String userId = principal.getName();
         playerService.updatePassword(userId, password);
